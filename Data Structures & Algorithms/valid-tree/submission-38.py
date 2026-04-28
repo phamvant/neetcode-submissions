763d139f25d19579bbc2612e563226cb51bcd38d
phamvant <44409501+phamvant@users.noreply.github.com>
2026-04-28 18:52:10 +0900

    Add: valid-tree - submission-38

---
+++ Data Structures & Algorithms/valid-tree/submission-38.py
@@ -0,0 +1,32 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        hashMap = {i: [] for i in range(n)}
+        visited = set()
+
+        for cur, nex in edges:
+            hashMap[cur].append(nex)
+            hashMap[nex].append(cur)
+
+        if len(edges) != n - 1:
+            return False
+
+        def dfs(node, par):
+            if node in visited:
+                return False
+            
+            visited.add(node)
+
+            for j in hashMap[node]:
+                if j == par:
+                    continue
+                if not dfs(j, node):
+                    return False
+
+            return True
+        
+        for i in range(n):
+            visited = set()
+            if dfs(i, i) and len(visited) == n:
+                return True
+
+        return False
