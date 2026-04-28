98987f5601a5cbf6c98686b154e04cee90d0b616
phamvant <44409501+phamvant@users.noreply.github.com>
2026-04-28 13:24:48 +0900

    Add: course-schedule - submission-30

---
+++ Data Structures & Algorithms/course-schedule/submission-30.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        hashMap = {i: [] for i in range(numCourses)}
+        for need, crs in prerequisites:
+            hashMap[crs].append(need)
+            
+        visited = set()
+
+        def dfs(i):
+            if i in visited:
+                return False
+
+            if len(hashMap.get(i, [])) == 0:
+                return True
+
+            visited.add(i)
+            for pre in hashMap[i]:
+                if not (dfs(pre)):
+                    return False
+            visited.remove(i)
+
+            return True
+
+        for i in range(numCourses):
+            if not (dfs(i)):
+                return False
+
+        return True
